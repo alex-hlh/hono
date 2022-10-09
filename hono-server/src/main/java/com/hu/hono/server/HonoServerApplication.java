08bdd22aef08e11b17f1dcd0e2ce843a97a125f3
hlh <hlh3727138@gmail.com>
2022-10-09 16:03:01 +0800

    start

---
+++ hono-server/src/main/java/com/hu/hono/server/HonoServerApplication.java
@@ -0,0 +1,13 @@
+package com.hu.hono.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HonoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HonoServerApplication.class, args);
+    }
+
+}
