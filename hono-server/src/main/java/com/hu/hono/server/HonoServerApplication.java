c394a6e8d92d373a352a0060e1204d14f7d4992d
hlh <hlh3727138@gmail.com>
2022-11-09 09:56:49 +0800

    banner添加

--- hono-server/src/main/java/com/hu/hono/server/HonoServerApplication.java
+++ hono-server/src/main/java/com/hu/hono/server/HonoServerApplication.java
@@ -1,13 +1,29 @@
 package com.hu.hono.server;
 
+import com.hu.hono.server.config.AppBanner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.util.StopWatch;
 
 @SpringBootApplication
 public class HonoServerApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(HonoServerApplication.class);
     public static void main(String[] args) {
-        SpringApplication.run(HonoServerApplication.class, args);
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        ConfigurableApplicationContext context = new SpringApplicationBuilder(HonoServerApplication.class)
+                .logStartupInfo(false)
+                .banner(new AppBanner())
+                .run(args);
+        stopWatch.stop();
+        Integer port = context.getBean(ServerProperties.class).getPort();
+        logger.info("HonoServer 服务启动完成，耗时:{}s，演示页请访问: http://127.0.0.1:{} ", stopWatch.getTotalTimeSeconds(), port);
     }
 
 }
