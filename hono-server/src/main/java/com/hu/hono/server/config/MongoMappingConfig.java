0c3bd175bb564f33ba4f28d48c5e3c20cd1849c1
hlh <hlh3727138@gmail.com>
2023-02-21 11:42:18 +0800

    mongodb添加

---
+++ hono-server/src/main/java/com/hu/hono/server/config/MongoMappingConfig.java
@@ -0,0 +1,45 @@
+package com.hu.hono.server.config;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.convert.CustomConversions;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+import org.springframework.data.mongodb.core.convert.DbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+
+/**
+ * @author hlh
+ * @version v1.0
+ * @title MongoMappingConfig
+ * @projectName hono
+ * @description
+ * @date 2023/2/21 11:10
+ */
+@Configuration
+public class MongoMappingConfig {
+
+    @Bean
+    public SimpleMongoClientDatabaseFactory getFactory() {
+        return new SimpleMongoClientDatabaseFactory("mongodb://hlh:123456@192.168.40.253:27017/hono?authSource=hono");
+    }
+
+    @Bean
+    public MappingMongoConverter mappingMongoConverter(SimpleMongoClientDatabaseFactory factory,
+                                                       MongoMappingContext context, BeanFactory beanFactory) {
+        DbRefResolver dbRefResolver = new DefaultDbRefResolver(factory);
+        MappingMongoConverter converter = new MappingMongoConverter(dbRefResolver, context);
+        try {
+            converter.setCustomConversions(beanFactory.getBean(CustomConversions.class));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //don't save column _class to mongo collection
+        converter.setTypeMapper(new DefaultMongoTypeMapper(null));
+        return converter;
+    }
+}
--- hono-server/src/main/java/com/hu/hono/server/controller/TestController.java
+++ hono-server/src/main/java/com/hu/hono/server/controller/TestController.java
@@ -1,17 +1,17 @@
 package com.hu.hono.server.controller;
 
-import com.hu.hono.server.service.DeleteEvent;
-import com.hu.hono.server.service.SaveEvent;
-import com.hu.hono.server.service.SelectEvent;
-import com.hu.hono.server.service.UpdateEvent;
+import cn.hutool.json.JSONUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author hlh
@@ -30,14 +30,30 @@ public class TestController {
     @Resource
     private ApplicationEventPublisher publisher;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/add")
-    public String test(){
-        log.info("==================start==========================");
-        publisher.publishEvent(new SaveEvent("save event"));
-        publisher.publishEvent(new DeleteEvent("delete event"));
-        publisher.publishEvent(new SelectEvent("select event"));
-        publisher.publishEvent(new UpdateEvent("update event"));
-        log.info("==================end==========================");
+    public String test() {
+        List<User> userList = new ArrayList<>();
+        User user1 = new User(1L, "user1", "1231", 12);
+        User user2 = new User(2L, "user2", "1232", 13);
+        User user3 = new User(3L, "user3", "1233", 14);
+        User user4 = new User(4L, "user4", "1234", 15);
+        User user5 = new User(5L, "user5", "1235", 16);
+        userList.add(user1);
+        userList.add(user2);
+        userList.add(user3);
+        userList.add(user4);
+        userList.add(user5);
+        userRepository.saveAll(userList);
         return "success";
     }
+
+    @GetMapping("/all")
+    public String all() {
+        List<User> all = userRepository.findAll();
+        return JSONUtil.toJsonStr(all);
+    }
+
 }
