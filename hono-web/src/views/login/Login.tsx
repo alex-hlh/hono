d2de7b348fca61ee23c031bc7193c0a5ed8145e8
hlh <hlh3727138@gmail.com>
2023-08-24 15:50:25 +0800

    react

--- hono-web/src/views/login/Login.tsx
+++ hono-web/src/views/login/Login.tsx
@@ -1,10 +1,17 @@
-import React from "react"
+import React, {useState} from "react"
 import "./index.less"
 
 const Login: React.FC = () => {
+	const [activie,setActive] = useState(false);
+	const handleSignUp = ()=>{
+		setActive(false)
+	}
+	const handleSignIn = ()=>{
+		setActive(true)
+	}
 	return (
 		<div className="box">
-			<div className="container" id="container">
+			<div className={["container", activie ? "right-panel-active":null].join(' ')} id="container" >
 				<div className="form-container sign-up-container">
 					<form action="#">
 						<h1>Create Account</h1>
@@ -23,7 +30,7 @@ const Login: React.FC = () => {
 						<input type="text" placeholder="Name" />
 						<input type="email" placeholder="Email" />
 						<input type="password" placeholder="Password" />
-						<button>Sign Up</button>
+						<button >Sign Up</button>
 					</form>
 				</div>
 				<div className="form-container sign-in-container">
@@ -44,7 +51,7 @@ const Login: React.FC = () => {
 						<input type="email" placeholder="Email" />
 						<input type="password" placeholder="Password" />
 						<a href="#">Forgot your password?</a>
-						<button>Sign In</button>
+						<button >Sign In</button>
 					</form>
 				</div>
 				<div className="overlay-container">
@@ -52,14 +59,14 @@ const Login: React.FC = () => {
 						<div className="overlay-panel overlay-left">
 							<h1>Welcome Back!</h1>
 							<p>To keep connected with us please login with your personal info</p>
-							<button className="ghost" id="signIn">
+							<button className="ghost" onClick={handleSignUp} id="signIn">
 								Sign In
 							</button>
 						</div>
 						<div className="overlay-panel overlay-right">
 							<h1>Hello, Friend!</h1>
 							<p>Enter your personal details and start journey with us</p>
-							<button className="ghost" id="signUp">
+							<button className="ghost" onClick={handleSignIn} id="signUp">
 								Sign Up
 							</button>
 						</div>
